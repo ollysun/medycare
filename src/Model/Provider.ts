53de854da15f0bd95532199fe22979a479b1c349
Moses <ollysun@gmail.com>
2017-07-28 10:55:42 +0100

    update provider html and the model

---
+++ src/Model/Provider.ts
@@ -0,0 +1,9 @@
+export class provider
+{
+    public title:string;
+    public name:string;
+    public username:string;
+    public email:string;
+    public password:string;
+    public Address:string;
+}
